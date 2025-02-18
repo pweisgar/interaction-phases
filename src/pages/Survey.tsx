06c226d5d276542cac595315bafef908024a8afc
Paul <paul.a.weisgarber@gmail.com>
2025-02-18 15:01:06 -0700

    20

--- src/pages/Survey.tsx
+++ src/pages/Survey.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import * as React from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { useSurvey } from '@/contexts/SurveyContext';
 import { Button } from "@/components/ui/button";
